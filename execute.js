b7c3e46641e0292d66aefb438458d917a26ebd9f
kpio7 <99970419+ni7x@users.noreply.github.com>
2023-10-17 21:00:51 +0200

    Scroll down and get info

--- execute.js
+++ execute.js
@@ -1,4 +1,22 @@
-// this code will be executed when the extension's button is clicked
-(function() {
-  console.log('execute.js executed');
+scrollToBottom = (currentHeight, callback) => {
+    const bottomElement = document.querySelector('footer._28xnwl[data-qa="footer"]');
+
+    if (bottomElement) {
+        bottomElement.scrollIntoView();
+        if (typeof callback === 'function') {
+            callback();
+        }
+    } else {
+        currentHeight += 350;
+        window.scrollTo(0, currentHeight);
+        setTimeout(() => scrollToBottom(currentHeight, callback), 40);
+    }
+}
+
+(() => {
+    scrollToBottom(0, () => {
+        var elements = document.getElementsByClassName('_2ro375');
+        console.log(elements);
+        console.log(elements.length);
+    });
 })();
