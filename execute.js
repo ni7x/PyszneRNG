b87ce410015dc91781972ffc0cdb785f74fcac6f
kpio7 <99970419+ni7x@users.noreply.github.com>
2023-10-17 22:07:00 +0200

    Ok finding restaurant working
    acceptable but not great

--- execute.js
+++ execute.js
@@ -2,21 +2,41 @@ scrollToBottom = (currentHeight, callback) => {
     const bottomElement = document.querySelector('footer._28xnwl[data-qa="footer"]');
 
     if (bottomElement) {
-        bottomElement.scrollIntoView();
+        bottomElement.scrollIntoView({"behavior":"instant"});
         if (typeof callback === 'function') {
             callback();
         }
     } else {
         currentHeight += 350;
         window.scrollTo(0, currentHeight);
-        setTimeout(() => scrollToBottom(currentHeight, callback), 40);
+        setTimeout(() => scrollToBottom(currentHeight, callback), 10);
     }
 }
 
 (() => {
     scrollToBottom(0, () => {
-        var elements = document.getElementsByClassName('_2ro375');
-        console.log(elements);
-        console.log(elements.length);
+        var restaurants = document.getElementsByClassName('_2ro375');
+
+        var openRestaurants = [];
+
+    
+        for (var i = 0; i < restaurants.length; i++) {
+            var isOpen = restaurants[i].querySelector('[data-qa="shipping-time-indicator-content"]');
+            var name = restaurants[i].querySelector("[data-qa]")
+           
+            if(isOpen.textContent != "Closed for delivery"){
+                if(name){
+                    openRestaurants.push(name);
+                    name.style.border = "";
+                }
+            }
+          }
+
+          var randomRestaurant = Math.floor(Math.random() * openRestaurants.length)
+      
+          openRestaurants[randomRestaurant >= 3 ? randomRestaurant - 3: 0].scrollIntoView({"behavior":"smooth"});
+        
+          openRestaurants[randomRestaurant].style.border = "4px solid red";
+
     });
 })();
